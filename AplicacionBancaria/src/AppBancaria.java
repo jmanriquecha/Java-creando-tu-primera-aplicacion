a09b9dd687eaf1ccad58ac797a8a1b2cf5e50658
Jorge Manrique <georgemanc.28@gmail.com>
2024-09-22 21:15:57 -0500

    Mejora en código, App bancaria, con consulta de saldo, retiros, deposito y salir del sistema

--- AplicacionBancaria/src/AppBancaria.java
+++ AplicacionBancaria/src/AppBancaria.java
@@ -12,6 +12,7 @@ public class AppBancaria {
         String detalleCuenta = null;
         String detalleOpcion = null;
         int opcionElegida = 0;
+        boolean flag = true;
 
         // Se crea el detalle de la cuenta
         detalleCuenta = """
@@ -34,37 +35,52 @@ public class AppBancaria {
                 4 - Salir
                 """;
 
-        while (true){
+        while (flag != false){
             // Imprime el detalle de la opción a elegir
             System.out.println(detalleOpcion);
             opcionElegida = scanner.nextInt();
 
             // Se realizan las respectivas validaciones
-            if(opcionElegida == 1){
-                System.out.println("El saldo actualizado es: $%.2f".formatted(saldo));
-            }else if (opcionElegida == 2){
-                System.out.println("¿Cuál es el valor que deseas retirar?");
-                double valorRetirar = scanner.nextDouble();
-                if (valorRetirar > saldo){
-                    System.out.println("Saldo insuficiente");
-                }else{
-                    saldo -= valorRetirar;
-                    System.out.println("Saldo restante $%.2f".formatted(saldo));
-                }
-            }else if (opcionElegida == 3){
-                System.out.println("¿Cuál es el valor que vas a depositar");
-                double valorDepositar = scanner.nextDouble();
-                if (valorDepositar <= 0){
-                    System.out.println("Ocurrio un error en la transacción!");
-                }else{
-                    saldo += valorDepositar;
+            switch (opcionElegida){
+                case 1:
                     System.out.println("El saldo actualizado es: $%.2f".formatted(saldo));
-                }
-            } else if (opcionElegida == 4) {
-                System.out.println("Finalizando el programa. Muchas gracias por usar nuestros servicios!");
-                break; // Finaliza el loop
-            } else{
-                System.out.println("¡No ha elegido una opción valida! por favor intente nuevamente");
+                    break;
+
+                case 2:
+                    System.out.println("¿Cuál es el valor que deseas retirar?");
+                    double valorRetirar = scanner.nextDouble();
+
+                    if (valorRetirar > 0) {
+                        if (valorRetirar > saldo) {
+                            System.out.println("Saldo insuficiente");
+                        } else {
+                            saldo -= valorRetirar;
+                            System.out.println("Saldo restante $%.2f".formatted(saldo));
+                        }
+                    }else{
+                        System.out.println("Ocurrio un error en la transacción!");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("¿Cuál es el valor que vas a depositar");
+                    double valorDepositar = scanner.nextDouble();
+                    if (valorDepositar <= 0){
+                        System.out.println("Ocurrio un error en la transacción!");
+                    }else{
+                        saldo += valorDepositar;
+                        System.out.println("El saldo actualizado es: $%.2f".formatted(saldo));
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Finalizando el programa. Muchas gracias por usar nuestros servicios!");
+                    flag = false; // Termina la ejecución del while
+                    break;
+
+                default:
+                    System.out.println("¡No ha elegido una opción valida! por favor intente nuevamente");
+                    break;
             }
         }
         scanner.close();
